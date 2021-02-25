77e71acf536657fc48dadd9c3350c4ac9c8d7413
hllbr <halibrahim.kocak@gmail.com>
2021-02-25 09:39:07 +0300

    SoutPark Layouts

---
+++ app/src/main/java/com/hllbr/layoutsoutpark/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hllbr.layoutsoutpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
